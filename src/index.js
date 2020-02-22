9669781ccc2652608c7cf17f9446a31421ebe6a6
github username <github email address>
2020-02-22 12:29:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitButton = document.getElementsByTagName('input')[1]
+  submitButton.id = 'submit'
+
+  document.addEventListener('click', event => {
+    const input = document.querySelector('#new-task-description')
+    const ul = document.querySelector('#list')
+    event.preventDefault()
+    
+    switch (true) {
+      case event.target.id === 'submit':
+       const li = document.createElement('li')
+        li.innerText = input.value
+        ul.append(li)
+    }
+  })
 });
